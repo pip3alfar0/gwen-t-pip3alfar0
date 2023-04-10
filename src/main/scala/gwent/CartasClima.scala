123a00a3669d65574a9ed01e4b3198d4ae9dc1c1
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-10 19:42:27 -0400

    FEAT: Added the trait Cartas. Added the classes CartasClima, CartasUnidad and Jugador. TEST: Added CartasTest and JugadorTest. DOC: Added @params for CartasClima, CartasUnidad and Jugador.

---
+++ src/main/scala/gwent/CartasClima.scala
@@ -0,0 +1,14 @@
+package cl.uchile.dcc
+package gwent
+
+/**
+ *
+ * @param nombre
+ * @param habilidad
+ * @return Carta
+ *
+ * */
+
+class CartasClima(nombre: String, habilidad: String) extends Cartas { //Weather cards get a name and an ability
+
+}
