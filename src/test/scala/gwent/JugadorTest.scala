8fc89ad546995fddcda7c413e6036ee49a0b0e10
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-22 23:19:51 -0400

    FEAT: modified the tests in JugadorTest with the new method equals

--- src/test/scala/gwent/JugadorTest.scala
+++ src/test/scala/gwent/JugadorTest.scala
@@ -2,10 +2,25 @@ package cl.uchile.dcc
 package gwent
 
 class JugadorTest extends munit.FunSuite{
-    //var Jugador1 = Jugador("Constanza", "bobaboba", 100, mazoCartas, manoCartas)
-    //var jugador2 = Jugador("Emilio", "industrial", 15, mazoCartas, manoCartas)
-    //var Jugador3 = Jugador("Diego", "blablabla", 22, mazoCartas, manoCartas)
-    //var jugador4 = Jugador("Agustín", "papitas", 20, mazoCartas, manoCartas)
-    //var jugador5 = Jugador("Cucha", "miau", 95, mazoCartas, manoCartas)
+  var jugador1: Jugador = null
+  var jugador2: Jugador = null
+  var jugador3: Jugador = null
+  var jugador4: Jugador = null
+  var jugador5: Jugador = null
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    jugador1 = new Jugador("Constanza", "bobaboba", 100, Set[Cartas](), Set[Cartas]())
+    jugador2 = new Jugador("Emilio", "industrial", 15, Set[Cartas](), Set[Cartas]())
+    jugador3 = new Jugador("Diego", "blablabla", 22, Set[Cartas](), Set[Cartas]())
+    jugador4 = new Jugador("Agustín", "papitas", 20, Set[Cartas](), Set[Cartas]())
+    jugador5 = new Jugador("Cucha", "miau", 95, Set[Cartas](), Set[Cartas]())
+  }
+
+  test("Equals"){
+    assert(!jugador1.equals(jugador2))
+    assert(!jugador3.equals(jugador4))
+    assert(!jugador5.equals(jugador2))
+    
+  }
     
 }
