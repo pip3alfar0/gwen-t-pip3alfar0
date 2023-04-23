50c52afbb7abe508f63aa64d775b3226479b1d13
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-22 22:48:35 -0400

    FEAT: changes cartas's trait for cartas's class

--- src/main/scala/gwent/Cartas.scala
+++ src/main/scala/gwent/Cartas.scala
@@ -1,6 +1,25 @@
 package cl.uchile.dcc
 package gwent
 
-trait Cartas {
-    
+/**
+ *
+ * @param nombre
+ * @param clasificacion
+ *
+ * */
+
+//Cards get a name and a classification
+class Cartas(private val nombre: String, private val clasificacion: String) { //The classification can be "Unity" or "Weather"
+  
+  
+  
+  override def equals(obj: Any): Boolean = {
+    if (obj.isInstanceOf[Cartas]) {
+      val other = obj.asInstanceOf[Cartas]
+      (this eq other) || other.nombre == nombre && other.clasificacion == clasificacion
+    } else {
+      false
+    }
+  }
+
 }
