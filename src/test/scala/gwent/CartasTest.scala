7dda82ed849e15ec08ab97f0973b7637e04a4dd1
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-22 23:19:04 -0400

    FEAT: modified the tests in CartasTest with the new method equals

--- src/test/scala/gwent/CartasTest.scala
+++ src/test/scala/gwent/CartasTest.scala
@@ -2,19 +2,43 @@ package cl.uchile.dcc
 package gwent
 
 class CartasTest extends munit.FunSuite {
-    var carta1 = new CartasClima("carta1", "a")
-    var carta2 = new CartasUnidad("carta2", "Asedio", 15)
-    var carta3 = new CartasUnidad("carta3", "Distancia", 25)
-    var carta4 = new CartasClima("carta4", "b")
-    var carta5 = new CartasUnidad("carta5", "Cuerpo a Cuerpo", 12)
+    var carta1: Cartas = null
+    var carta2: Cartas = null
+    var carta3: Cartas = null
+    var carta4: Cartas = null
+    var carta5: Cartas = null
+
+    override def beforeEach(context: BeforeEach): Unit = {
+        carta1 = new Cartas("carta1", "Clima")
+        carta2 = new Cartas("carta2", "Unidad")
+        carta3 = new Cartas("carta3", "Unidad")
+        carta4 = new Cartas("carta4", "Clima")
+        carta5 = new Cartas("carta5", "Unidad")
+    }
 
     test("Equals"){
-        assertEquals(carta1,carta1)
-        assertEquals(carta4,carta4)
-        assertNotEquals(carta1,carta4)
-        assertNotEquals(carta2,carta3)
-        assertNotEquals(carta3,carta5)
+        carta1.equals(carta1)
+        carta2.equals(carta2)
+        carta3.equals(carta3)
+        carta4.equals(carta4)
+        carta5.equals(carta5)
+
+        assert(!carta1.equals(carta2))
+        assert(!carta1.equals(carta3))
+        assert(!carta1.equals(carta4))
+        assert(!carta1.equals(carta5))
+        assert(!carta4.equals(carta5))
+
+        assertEquals(carta1, new Cartas("carta1", "Clima"))
+        assertEquals(carta3, new Cartas("carta3", "Unidad"))
+        assertEquals(carta1, carta1)
+        assertEquals(carta4, carta4)
+        assertNotEquals(carta1, carta4)
+        assertNotEquals(carta2, carta3)
+        assertNotEquals(carta3, carta5)
         assert(!carta3.equals(carta4))
         assert(!carta1.equals(carta5))
     }
+
+
 }
