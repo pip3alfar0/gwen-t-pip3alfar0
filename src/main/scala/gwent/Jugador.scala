e559275ef3e5511b5e82577a7b2b17e6a500d9bb
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-22 22:53:17 -0400

    FEAT: overrides method equals in Jugador class

--- src/main/scala/gwent/Jugador.scala
+++ src/main/scala/gwent/Jugador.scala
@@ -12,16 +12,29 @@ package gwent
  * */
 
 //the class player get a name, a section on the board, a gem counter (player's life), a card deck and a card hand
-class Jugador(nombre: String, seccionTablero: String, contadorGemas: Int, mazoCartas: Set[Cartas], manoCartas: Set[Cartas]) {
+class Jugador(private val nombre: String, private val seccionTablero: String, private val contadorGemas: Int, private val mazo: Set[Cartas], private val manoCartas: Set[Cartas]) {
 
     //Select a card from your hand and place it on the board to perform an action
-    def Jugar(): Unit = { 
+    /*
+    def Jugar(): Unit = {
+
+    }
+    */
+
+    //Take a card from the deck and add it to your hand
+    def Robar(): Unit = {
 
     }
 
-    //Take a card from the deck and add it to your hand
-    def Robar(): Unit = { 
-
+    //Overriding the function equals for Jugador
+    override def equals(obj: Any): Boolean = {
+        if (obj.isInstanceOf[Jugador]) {
+            val other = obj.asInstanceOf[Jugador]
+            (this eq other) || other.nombre == nombre && other.seccionTablero == seccionTablero && other.contadorGemas == contadorGemas
+            && other.mazo == mazo && other.manoCartas == manoCartas
+        } else {
+            false
+        }
     }
 
 }
