79490cabd526ff53b9ae77f585406bfa85a8cf6a
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-22 23:17:36 -0400

    FEAT: overrides method hashCode in class Jugador

--- src/main/scala/gwent/Jugador.scala
+++ src/main/scala/gwent/Jugador.scala
@@ -1,7 +1,7 @@
 package cl.uchile.dcc
 package gwent
 
-/**
+/** It's a class that represents a player in the game
  *
  * @param nombre
  * @param seccionTablero
@@ -12,18 +12,23 @@ package gwent
  * */
 
 //the class player get a name, a section on the board, a gem counter (player's life), a card deck and a card hand
-class Jugador(private val nombre: String, private val seccionTablero: String, private val contadorGemas: Int, private val mazo: Set[Cartas], private val manoCartas: Set[Cartas]) {
+class Jugador(private val nombre: String, private val seccionTablero: String, private val contadorGemas: Int, private var mazo: Set[Cartas], private var manoCartas: Set[Cartas]) {
 
     //Select a card from your hand and place it on the board to perform an action
     /*
     def Jugar(): Unit = {
-
     }
     */
 
     //Take a card from the deck and add it to your hand
     def Robar(): Unit = {
+        manoCartas = manoCartas + mazo.head
+        mazo = mazo - mazo.head
+    }
 
+    //Overriding the function hashCode for Jugador
+    override def hashCode(): Int = {
+        Objects.hash(classOf[Jugador], nombre, seccionTablero, contadorGemas, mazo, manoCartas)
     }
 
     //Overriding the function equals for Jugador
