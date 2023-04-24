f4569be65104e82de432d24721707ce077375264
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-24 19:42:12 -0400

    DELETE: deleted the abstract class AbstractCartas

--- src/main/scala/gwent/Cartas.scala
+++ src/main/scala/gwent/Cartas.scala
@@ -1,32 +1,7 @@
 package cl.uchile.dcc
 package gwent
-import java.util.Objects
 
-/** It's a class that represents a card of the game
- *
- * @param nombre
- * @param clasificacion
- *
+/** It's a trait that represents a card of the game.
  * */
 
-//Cards get a name and a classification
-class Cartas(private val nombre: String, private val clasificacion: String) { //The classification can be "Unity" or "Weather"
-
-
-
-  //Overriding the function hashCode for Cartas
-  override def hashCode(): Int = {
-    Objects.hash(classOf[Cartas], nombre, clasificacion)
-  }
-
-  //Overriding the function equals for Cartas
-  override def equals(obj: Any): Boolean = {
-    if (obj.isInstanceOf[Cartas]) {
-      val other = obj.asInstanceOf[Cartas]
-      (this eq other) || other.nombre == nombre && other.clasificacion == clasificacion
-    } else {
-      false
-    }
-  }
-
-}
+trait Cartas
