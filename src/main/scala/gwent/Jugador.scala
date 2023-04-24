0aedbe3437efb75d20ab773f2f89189acfc2dd4c
pip3alfar0 <alfaro.feli@gmail.com>
2023-04-24 19:46:10 -0400

    FEAT: added documentation for Jugador's class and for Jugador's Robar function

--- src/main/scala/gwent/Jugador.scala
+++ src/main/scala/gwent/Jugador.scala
@@ -2,18 +2,24 @@ package cl.uchile.dcc
 package gwent
 import java.util.Objects
 
-/** It's a class that represents a player in the game
+/** It's a class that represents a player in the game.
+ * This player is denifed by a name, a board section, a gem counter, a deck of cards and a hand of cards.
  *
- * @param nombre
- * @param seccionTablero
- * @param contadorGemas
- * @param mazoCartas
- * @param manoCartas
+ * @param nombre The name of the Player.
+ * @param seccionTablero The board section of the Player.
+ * @param contadorGemas The gem counter, represents the Player's life.
+ * @param mazoCartas The deck of cards are the cards in the board.
+ * @param manoCartas The hand of cards are the Player's cards.
  *
+ * @example
+ * {{{
+ *   jugador1 = new Jugador("Constanza", "bobaboba", 100, Set[Cartas](new CartaUnidad("carta1", "Asedio", 50)), Set[Cartas]())
+ * }}}
+ *
+ * @author Felipe Alfaro
  * */
 
-//the class player get a name, a section on the board, a gem counter (player's life), a card deck and a card hand
-class Jugador(private val nombre: String, private val seccionTablero: String, private val contadorGemas: Int, private var mazo: Set[Cartas], private var manoCartas: Set[Cartas]) {
+class Jugador(val nombre: String, val seccionTablero: String, val contadorGemas: Int, var mazo: Set[Cartas], var manoCartas: Set[Cartas]) {
 
     //Select a card from your hand and place it on the board to perform an action
     /*
@@ -21,7 +27,19 @@ class Jugador(private val nombre: String, private val seccionTablero: String, pr
     }
     */
 
-    //Take a card from the deck and add it to your hand
+    /** Take a card from the deck and add it to your hand.
+     *
+     * Steal a card of the deck is defined as the original deck of cards less the stolen card and now the hand of
+     * Player's cards are the same cards with the stolen deck's card.
+     *
+     * This function modifies the deck of cards and the hand of cards when the card is drawn.
+     *
+     * @example
+     * {{{
+     *  val jugador1 = new Jugador("Constanza", "bobaboba", 100, Set[Cartas](), Set[Cartas](new CartaUnidad("carta1", "Asedio", 50)))
+     *  jugador1.Robar()
+     * }}}
+     */
     def Robar(): Unit = {
         manoCartas = manoCartas + mazo.head
         mazo = mazo - mazo.head
@@ -32,12 +50,11 @@ class Jugador(private val nombre: String, private val seccionTablero: String, pr
         Objects.hash(classOf[Jugador], nombre, seccionTablero, contadorGemas, mazo, manoCartas)
     }
 
-    //Overriding the function equals for Jugador
+    //Overriding the function equals for Cartas
     override def equals(obj: Any): Boolean = {
         if (obj.isInstanceOf[Jugador]) {
             val other = obj.asInstanceOf[Jugador]
-            (this eq other) || other.nombre == nombre && other.seccionTablero == seccionTablero && other.contadorGemas == contadorGemas
-            && other.mazo == mazo && other.manoCartas == manoCartas
+            (this eq other) || other.nombre == nombre && other.seccionTablero == seccionTablero && other.contadorGemas == contadorGemas && other.mazo == mazo && other.manoCartas == manoCartas
         } else {
             false
         }
