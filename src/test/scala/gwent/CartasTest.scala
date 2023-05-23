b6dc20a53a2c700ccdb5ab757c4a99f0af9d8afc
pip3alfar0 <alfaro.feli@gmail.com>
2023-05-22 21:13:31 -0400

    TEST: added more tests to CartasTest

--- src/test/scala/gwent/CartasTest.scala
+++ src/test/scala/gwent/CartasTest.scala
@@ -7,20 +7,42 @@ class CartasTest extends munit.FunSuite {
     val nombre3: String = "carta3"
     val nombre4: String = "carta4"
     val nombre5: String = "carta5"
+    val nombre6: String = "carta6"
+    val nombre7: String = "carta7"
+    val nombre8: String = "carta8"
 
-    var carta1: CartaUnidad = _  // null
+    val descripcion1: String = "Asedio"
+    val descripcion2: String = "a"
+    val descripcion3: String = "Cuerpo a Cuerpo"
+    val descripcion4: String = "b"
+    val descripcion5: String = "Distancia"
+
+    val fuerza1: Int = 50
+    val fuerza2: Int = 90
+    val fuerza3: Int = 35
+    val fuerza4: Int = 70
+    val fuerza5: Int = 45
+    val fuerza6: Int = 25
+
+    var carta1: Asedio = _  // null
     var carta2: CartaClima = _
-    var carta3: CartaUnidad = _
-    var carta4: CartaUnidad = _
+    var carta3: CuerpoACuerpo = _
+    var carta4: Distancia = _
     var carta5: CartaClima = _
-    
-    
+    var carta6: Asedio = _
+    var carta7: CuerpoACuerpo = _
+    var carta8: Distancia = _
+
+
     override def beforeEach(context: BeforeEach): Unit = {
-        carta1 = new CartaUnidad("carta1", "Asedio", 50)
+        carta1 = new Asedio("carta1", "Asedio", 50)
         carta2 = new CartaClima("carta2", "a")
-        carta3 = new CartaUnidad("carta3", "Cuerpo a Cuerpo", 90)
-        carta4 = new CartaUnidad("carta4", "Distancia", 35)
+        carta3 = new CuerpoACuerpo("carta3", "Cuerpo a Cuerpo", 90)
+        carta4 = new Distancia("carta4", "Distancia", 35)
         carta5 = new CartaClima("carta5", "b")
+        carta6 = new Asedio("carta6", "Asedio", 70)
+        carta7 = new CuerpoACuerpo("carta7", "Cuerpo a Cuerpo", 45)
+        carta8 = new Distancia("carta8", "Distancia", 25)
     }
 
     test("Equals"){
@@ -29,25 +51,64 @@ class CartasTest extends munit.FunSuite {
         carta3.equals(carta3)
         carta4.equals(carta4)
         carta5.equals(carta5)
+        carta6.equals(carta6)
+        carta7.equals(carta7)
+        carta8.equals(carta8)
 
+        assertEquals(carta1, new Asedio("carta1", "Asedio", 50))
+        assertEquals(carta2, new CartaClima("carta2", "a"))
+        assertEquals(carta3, new CuerpoACuerpo("carta3", "Cuerpo a Cuerpo", 90))
+        assertEquals(carta4, new Distancia("carta4", "Distancia", 35))
+        assertEquals(carta5, new CartaClima("carta5", "b"))
+        assertEquals(carta6, new Asedio("carta6", "Asedio", 70))
+        assertEquals(carta7, new CuerpoACuerpo("carta7", "Cuerpo a Cuerpo", 45))
+        assertEquals(carta8, new Distancia("carta8", "Distancia", 25))
+
+        assert(!carta1.equals(new CartaClima("carta2", "a")))
+        assert(!carta2.equals(new CuerpoACuerpo("carta3", "Cuerpo a Cuerpo", 90)))
+        assert(!carta3.equals(new Distancia("carta4", "Distancia", 35)))
+        assert(!carta4.equals(new CartaClima("carta5", "b")))
+        assert(!carta5.equals(new Asedio("carta6", "Asedio", 70)))
+        assert(!carta6.equals(new CuerpoACuerpo("carta7", "Cuerpo a Cuerpo", 45)))
+        assert(!carta7.equals(new Distancia("carta8", "Distancia", 25)))
+        assert(!carta8.equals(new Asedio("carta1", "Asedio", 50)))
+
+        assertEquals(carta1, carta1)
+        assertEquals(carta2, carta2)
+        assertEquals(carta3, carta3)
+        assertEquals(carta4, carta4)
+        assertEquals(carta5, carta5)
+        assertEquals(carta6, carta6)
+        assertEquals(carta7, carta7)
+        assertEquals(carta8, carta8)
         assert(!carta1.equals(carta2))
         assert(!carta1.equals(carta3))
         assert(!carta1.equals(carta4))
         assert(!carta1.equals(carta5))
-        assert(!carta4.equals(carta5))
-
-        assertEquals(carta1, new CartaUnidad("carta1", "Asedio", 50))
-        assertEquals(carta2, new CartaClima("carta2", "a"))
-        assertEquals(carta3, new CartaUnidad("carta3", "Cuerpo a Cuerpo", 90))
-        assertEquals(carta1, carta1)
-        assertEquals(carta4, carta4)
-        assertNotEquals(carta1, carta4)
-        assertNotEquals(carta3, carta4)
-        assertNotEquals(carta2, carta5)
+        assert(!carta1.equals(carta6))
+        assert(!carta1.equals(carta7))
+        assert(!carta1.equals(carta8))
+        assert(!carta2.equals(carta3))
+        assert(!carta2.equals(carta4))
+        assert(!carta2.equals(carta5))
+        assert(!carta2.equals(carta6))
+        assert(!carta2.equals(carta7))
+        assert(!carta2.equals(carta8))
         assert(!carta3.equals(carta4))
-        assert(!carta1.equals(carta5))
-        assert(!carta1.equals(carta2))
         assert(!carta3.equals(carta5))
+        assert(!carta3.equals(carta6))
+        assert(!carta3.equals(carta7))
+        assert(!carta3.equals(carta8))
+        assert(!carta4.equals(carta5))
+        assert(!carta4.equals(carta6))
+        assert(!carta4.equals(carta7))
+        assert(!carta4.equals(carta8))
+        assert(!carta5.equals(carta6))
+        assert(!carta5.equals(carta7))
+        assert(!carta5.equals(carta8))
+        assert(!carta6.equals(carta7))
+        assert(!carta6.equals(carta8))
+        assert(!carta7.equals(carta8))
     }
 
     test("hashCode"){
@@ -55,26 +116,91 @@ class CartasTest extends munit.FunSuite {
         assert(carta2.hashCode()!=carta3.hashCode())
         assert(carta3.hashCode()!=carta4.hashCode())
         assert(carta4.hashCode()!=carta5.hashCode())
-        assert(carta5.hashCode()!=carta1.hashCode())
+        assert(carta5.hashCode()!=carta6.hashCode())
+        assert(carta6.hashCode()!=carta7.hashCode())
+        assert(carta7.hashCode()!=carta8.hashCode())
+        assert(carta8.hashCode()!=carta1.hashCode())
         
-        assert(carta1.hashCode() == (new CartaUnidad("carta1", "Asedio", 50)).hashCode())
+        assert(carta1.hashCode() == (new Asedio("carta1", "Asedio", 50)).hashCode())
         assert(carta2.hashCode() == (new CartaClima("carta2", "a")).hashCode())
-        assert(carta3.hashCode() == (new CartaUnidad("carta3", "Cuerpo a Cuerpo", 90)).hashCode())
-        assert(carta4.hashCode() == (new CartaUnidad("carta4", "Distancia", 35)).hashCode())
+        assert(carta3.hashCode() == (new CuerpoACuerpo("carta3", "Cuerpo a Cuerpo", 90)).hashCode())
+        assert(carta4.hashCode() == (new Distancia("carta4", "Distancia", 35)).hashCode())
         assert(carta5.hashCode() == (new CartaClima("carta5", "b")).hashCode())
+        assert(carta6.hashCode() == (new Asedio("carta6", "Asedio", 70)).hashCode())
+        assert(carta7.hashCode() == (new CuerpoACuerpo("carta7", "Cuerpo a Cuerpo", 45)).hashCode())
+        assert(carta8.hashCode() == (new Distancia("carta8", "Distancia", 25)).hashCode())
     }
 
-    test("Nombres") {
+    test("Nombre") {
         assertEquals(carta1.nombre, nombre1)
         assertEquals(carta2.nombre, nombre2)
         assertEquals(carta3.nombre, nombre3)
         assertEquals(carta4.nombre, nombre4)
         assertEquals(carta5.nombre, nombre5)
+        assertEquals(carta6.nombre, nombre6)
+        assertEquals(carta7.nombre, nombre7)
+        assertEquals(carta8.nombre, nombre8)
+
+        assert(!carta1.nombre.equals(nombre2))
+        assert(!carta2.nombre.equals(nombre3))
+        assert(!carta3.nombre.equals(nombre4))
+        assert(!carta4.nombre.equals(nombre5))
+        assert(!carta5.nombre.equals(nombre6))
+        assert(!carta6.nombre.equals(nombre7))
+        assert(!carta7.nombre.equals(nombre8))
+        assert(!carta8.nombre.equals(nombre1))
+    }
+
+    test("Descripcion") {
+        assertEquals(carta1.descripcion, descripcion1)
+        assertEquals(carta2.descripcion, descripcion2)
+        assertEquals(carta3.descripcion, descripcion3)
+        assertEquals(carta4.descripcion, descripcion5)
+        assertEquals(carta5.descripcion, descripcion4)
+        assertEquals(carta6.descripcion, descripcion1)
+        assertEquals(carta7.descripcion, descripcion3)
+        assertEquals(carta8.descripcion, descripcion5)
+
+        assert(!carta1.nombre.equals(descripcion2))
+        assert(!carta2.nombre.equals(descripcion3))
+        assert(!carta3.nombre.equals(descripcion4))
+        assert(!carta4.nombre.equals(descripcion2))
+        assert(!carta5.nombre.equals(descripcion3))
+        assert(!carta6.nombre.equals(descripcion4))
+        assert(!carta7.nombre.equals(descripcion5))
+        assert(!carta8.nombre.equals(descripcion1))
+    }
+
+    test("Fuerza") {
+        assertEquals(carta1.fuerza, fuerza1)
+        assertEquals(carta3.fuerza, fuerza2)
+        assertEquals(carta4.fuerza, fuerza3)
+        assertEquals(carta6.fuerza, fuerza4)
+        assertEquals(carta7.fuerza, fuerza5)
+        assertEquals(carta8.fuerza, fuerza6)
+
+        assert(!carta1.fuerza.equals(fuerza2))
+        assert(!carta3.fuerza.equals(fuerza3))
+        assert(!carta4.fuerza.equals(fuerza4))
+        assert(!carta6.fuerza.equals(fuerza5))
+        assert(!carta7.fuerza.equals(fuerza6))
+        assert(!carta8.fuerza.equals(fuerza1))
+    }
+
+    test("Definiciones") {
+        assert(!carta1.fuerza.equals(descripcion1))
+        assert(!carta2.nombre.equals(fuerza6))
+        assert(!carta3.fuerza.equals(nombre3))
+        assert(!carta4.fuerza.equals(descripcion2))
+        assert(!carta5.descripcion.equals(descripcion1))
+        assert(!carta6.nombre.equals(fuerza4))
+        assert(!carta7.fuerza.equals(descripcion3))
+        assert(!carta8.descripcion.equals(fuerza2))
     }
 
     test("Jogador") {
-        assert(!carta1.equals(new Jugador("Simba", "miau miau", 90, Set[Cartas](), Set[Cartas]())))
-        assert(!carta2.equals(new Jugador("Kitty", "miau miau miau", 100, Set[Cartas](), Set[Cartas]())))
+        assert(!carta1.equals(new Jugador("Simba", "miau miau", 90, List[Cartas](), List[Cartas]())))
+        assert(!carta2.equals(new Jugador("Kitty", "miau miau miau", 100, List[Cartas](), List[Cartas]())))
     }
 
 }
