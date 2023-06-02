65c63c12733dab86f6477b2cd7527c0223c1bbf1
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-01 22:07:04 -0400

    FEAT: added the class SeccionTablero with the methods jugarDistancia, jugarCuerpoACuerpo and jugarAsedio

--- src/main/scala/gwent/Jugador/Jugador.scala
+++ src/main/scala/gwent/Jugador/Jugador.scala
@@ -1,13 +1,14 @@
 package cl.uchile.dcc
-package gwent
-import cl.uchile.dcc.gwent.Cartas.Cartas
+package gwent.Jugador
 
+import gwent.Cartas.Cartas
+import gwent.Tablero.{SeccionTablero, Tablero}
 import java.util.Objects
 
 /** It's a class that represents a player in the game.
  * This player is defined by a name, a board section, a gem counter, a deck of cards and a hand of cards.
  *
- * The deck and hand are private variables, but can be accessed via their corresponding
+ * The gem counter, deck and hand are private variables, but can be accessed via their corresponding
  * getter methods.
  * We use immutable lists instead of mutable ones to represent the deck and the hand.
  * This is a common practice in functional programming and in Scala in particular.
@@ -22,13 +23,17 @@ import java.util.Objects
  *
  * @example
  * {{{
- *      jugador1 = new Jugador("Constanza", "bobaboba", 100, Set[Cartas](new Asedio("carta1", "Asedio", 50)), Set[Cartas]())
+ *      jugador = new Jugador("Constanza", new seccionTablero(List[Cartas](carta1), List[Cartas](carta3), List[Cartas](carta10, carta12)), 100, List[Cartas](carta2), List[Cartas]())
  * }}}
  *
  * @author Felipe Alfaro
  * */
 
-class Jugador(val nombre: String, val seccionTablero: String, val contadorGemas: Int, private var _mazo: List[Cartas], private var _manoCartas: List[Cartas]) {
+class Jugador(val nombre: String, var seccionTablero: SeccionTablero, private val _contadorGemas: Int, private var _mazo: List[Cartas],
+              private var _manoCartas: List[Cartas]) {
+
+    /** Accesor methos for the player's gem counter */
+    def contadorGemas: Int = _contadorGemas
 
     /** Accessor method for the player's deck */
     def mazo: List[Cartas] = _mazo
@@ -36,25 +41,26 @@ class Jugador(val nombre: String, val seccionTablero: String, val contadorGemas:
     /** Accessor method for the player's hand */
     def manoCartas: List[Cartas] = _manoCartas
 
+    /** The gem counter can not be negative */
+    require(contadorGemas >= 0, "The gem counter can not be less than zero")
+
     /** Select a card from your hand and place it on the board to perform an action.
      *
-     * Play a card of the hand is defined as the original hand of cards less the played card and now the deck of
-     * cards are the same cards with the played card.
+     * Play a card of the hand is defined as the original hand of cards less the played card and now the card's section 
+     * are the same cards with the played card.
      *
-     * This function modifies the deck of cards and the hand of cards when the card is drawn.
-     * 
+     * This function modifies the player's section on the board or the weather zone and the hand of cards when the card is drawn.
+     *
      * @param card The played card.
-     *
+     * @param board The board where the player is playing.
      * @example
-     * {{{
-     *  val jugador1 = new Jugador("Constanza", "1", 100, List[Cartas](carta1), List[Cartas](carta3, carta4))
-     *  jugador1.Jugar(carta3)
-     *  val expected = new Jugador("Constanza", "1", 100, List[Cartas](carta1), List[Cartas](carta4))
-     * }}}
+     * 
+     * @author Felipe Alfaro
      */
-    def Jugar(card: Cartas): Unit = {
-        _manoCartas = _manoCartas.filter(_ != card)
-        card.Jugar(this) // this: player
+    def Jugar(card: Cartas, board: Tablero): Unit = {
+        require(manoCartas.contains(card), "The card is not on the player's hand")
+        _manoCartas = manoCartas.filterNot(_ eq card)
+        card.Jugar(this, board)
     }
     
     /** Take a card from the deck and add it to your hand.
--- src/main/scala/gwent/Tablero/SeccionTablero.scala
+++ src/main/scala/gwent/Tablero/SeccionTablero.scala
@@ -1,5 +1,57 @@
-package cl.uchile.dcc.gwent.Tablero
+package cl.uchile.dcc
+package gwent.Tablero
 
-class seccionTablero {
+import gwent.Cartas.Cartas
+import gwent.Cartas.Unidad.{Asedio, CuerpoACuerpo, Distancia}
 
+/** It's a class that represents a the player's section of the board. 
+ * 
+ * This player's section board is defined by a list of siege combat cards, a list of close combat cards and a list of ranged combat cards.
+ *
+ * @param distancia The list of ranged combat cards.
+ * @param asedio The list of siege combat cards.
+ * @param cuerpoACuerpo The list of close combat cards.
+ *
+ * @author Felipe Alfaro
+ * */
+
+class SeccionTablero(var distancia: List[Distancia] = List(), var asedio: List[Asedio] = List(), 
+                     var cuerpoACuerpo: List[CuerpoACuerpo] = List()) {
+
+  /** Play a ranged combat card and added to the ranged combat card zone.
+   *
+   * Play a ranged combat card and added to the ranged combat card zone is defined as the original list of ranged combat cards in that zone plus the played ranged combat card.
+   *
+   * This function modifies the list of ranged combat cards that are in the ranged combat zone.
+   *
+   * @param carta The played ranged combat card that will be added to the ranged combat zone.
+   */
+  def jugarDistancia(carta: Distancia): Unit = {
+    distancia = carta :: distancia
+  }
+
+  /** Play a siege combat card and added to the siege combat card zone.
+   *
+   * Play a siege combat card and added to the siege combat card zone is defined as the original list of siege combat cards in that zone plus the played siege combat card.
+   *
+   * This function modifies the list of siege combat cards that are in the siege combat zone.
+   *
+   * @param carta The played siege combat card that will be added to the siege combat zone.
+   */
+  def jugarAsedio(carta: Asedio): Unit = {
+    asedio = carta :: asedio
+  }
+
+  /** Play a close combat card and added to the close combat card zone.
+   *
+   * Play a close combat card and added to the close combat card zone is defined as the original list of close combat cards in that zone plus the played close combat card.
+   *
+   * This function modifies the list of close combat cards that are in the close combat zone.
+   *
+   * @param carta The played close combat card that will be added to the close combat zone.
+   */
+  def jugarCuerpoACuerpo(carta: CuerpoACuerpo): Unit = {
+    cuerpoACuerpo = carta :: cuerpoACuerpo
+  }
+  
 }
