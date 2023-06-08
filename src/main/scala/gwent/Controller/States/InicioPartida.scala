a9eb5c7cfb4fe7f82c5002e44c75b132a644c32c
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-07 21:28:56 -0400

    FEAT: created the class InicioPartida with the override methods toJugadorJuega and toComputadorJuega

--- src/main/scala/gwent/Controller/States/InicioPartida.scala
+++ src/main/scala/gwent/Controller/States/InicioPartida.scala
@@ -1,5 +1,32 @@
-package cl.uchile.dcc.gwent.Controller.States
+package cl.uchile.dcc
+package gwent.Controller.States
 
-class InicioPartida {
+import gwent.Jugador.Jugador
+import gwent.Controller.GameController
+
+
+/** Class representing the start of the game.
+ *
+ * A `StartGame` is a type of [[EstadoJuego]].
+ * This start represent the first things to do in the game.
+ *
+ * @param context The context of the game.
+ * @author Felipe Alfaro
+ */
+class InicioPartida(context: GameController) extends EstadoJuego(context) {
+
+  /** The player decides which card it's going to be played */
+  override def toJugadorJuega(): Unit = {
+    context.estado = new JugadorJuega(context)
+    //context.jugar()
+  }
+
+  /** debe jugar aleatoriamente*/
+  override def toComputadorJuega(): Unit = {
+    context.estado = new ComputadorJuega(context)
+    //context.jugar()
+  }
 
 }
+
+
