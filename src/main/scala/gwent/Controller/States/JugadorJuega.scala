3543365e9cbfa75cb9484d28918e049dc46139d0
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-07 21:30:09 -0400

    FEAT: created the class JugadorJuega as a game state, with the override methods toOtroJuega, toComputadorJuega and doAction

--- src/main/scala/gwent/Controller/States/JugadorJuega.scala
+++ src/main/scala/gwent/Controller/States/JugadorJuega.scala
@@ -1,5 +1,22 @@
-package cl.uchile.dcc.gwent.Controller.States
+package cl.uchile.dcc
+package gwent.Controller.States
 
-class JugadorJuega {
+import gwent.Controller.GameController
+
+class JugadorJuega(context: GameController) extends EstadoJuego(context) {
+
+  override def toOtroJuega(): Unit = {
+    context.estado = new OtroJuega(context)
+  }
+
+  override def toComputadorJuega(): Unit = {
+    context.estado = new ComputadorJuega(context)
+  }
+
+  // tiene que jugar una carta
+  override def doAction(): Unit = {
+    context.jugarPlayer()
+  }
 
 }
+
