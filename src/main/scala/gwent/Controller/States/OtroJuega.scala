29cbda564a110569655ee8db9ce037ead5b847ff
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-07 21:30:51 -0400

    FEAT: created the class OtroJuega as a game state, with the override methods toFinalRound, toComputadorJuega and doAction

--- src/main/scala/gwent/Controller/States/OtroJuega.scala
+++ src/main/scala/gwent/Controller/States/OtroJuega.scala
@@ -1,5 +1,17 @@
-package cl.uchile.dcc.gwent.Controller.States
+package cl.uchile.dcc
+package gwent.Controller.States
 
-class OtroJuega {
+import gwent.Controller.GameController
+
+class OtroJuega(context: GameController) extends EstadoJuego(context) {
+
+  override def toFinalRound(): Unit = {
+    context.estado = new FinalRound(context)
+  }
+
+  override def doAction(): Unit = {
+    //context.jugar()
+  }
 
 }
+
