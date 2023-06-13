9b2734cdc00a25d988b7a48b59b301eb0521bc0a
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-13 18:21:18 -0400

    FEAT: changed the language from spanish to english

--- src/main/scala/gwent/Controller/States/FinalRound.scala
+++ src/main/scala/gwent/Controller/States/FinalRound.scala
@@ -3,14 +3,14 @@ package gwent.Controller.States
 
 import gwent.Controller.GameController
 
-class FinalRound(context: GameController) extends EstadoJuego(context) {
+class FinalRound(context: GameController) extends GameState(context) {
 
-  override def toFinPartida(): Unit = {
-    context.estado = new FinPartida(context)
+  override def toEndGame(): Unit = {
+    context.state = new EndGame(context)
   }
 
-  override def toOtroRound(): Unit = {
-    context.estado = new OtroRound(context)
+  override def toAnotherRound(): Unit = {
+    context.state = new AnotherRound(context)
   }
 
 }
