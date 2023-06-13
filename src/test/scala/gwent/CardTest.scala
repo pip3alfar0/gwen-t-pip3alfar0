9e28d15808bad706f8fcf953a9698ef6e163a6b7
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-13 18:22:36 -0400

    FEAT: changed the language from spanish to english

--- src/test/scala/gwent/CardTest.scala
+++ src/test/scala/gwent/CardTest.scala
@@ -1,212 +1,212 @@
 package cl.uchile.dcc
 package gwent
 
-import gwent.Cartas.{WeatherCard, Card}
+import gwent.Cards.{WeatherCard, Card}
 
-import cl.uchile.dcc.gwent.Cartas.Unidad.{SiegeCombatCard, CloseCombatCard, RangedCombatCard}
-import cl.uchile.dcc.gwent.Jugador.Player
-import cl.uchile.dcc.gwent.Tablero.boardSection
+import cl.uchile.dcc.gwent.Cards.Unit.{SiegeCombatCard, CloseCombatCard, RangedCombatCard}
+import cl.uchile.dcc.gwent.Player.Player
+import cl.uchile.dcc.gwent.Board.boardSection
 
-class CartasTest extends munit.FunSuite {
-    val nombre1: String = "carta1"
-    val nombre2: String = "carta2"
-    val nombre3: String = "carta3"
-    val nombre4: String = "carta4"
-    val nombre5: String = "carta5"
-    val nombre6: String = "carta6"
-    val nombre7: String = "carta7"
-    val nombre8: String = "carta8"
+class CardTest extends munit.FunSuite {
+    val name1: String = "card1"
+    val name2: String = "card2"
+    val name3: String = "card3"
+    val name4: String = "card4"
+    val name5: String = "card5"
+    val name6: String = "card6"
+    val name7: String = "card7"
+    val name8: String = "card8"
 
-    val descripcion1: String = "Asedio"
-    val descripcion2: String = "a"
-    val descripcion3: String = "Cuerpo a Cuerpo"
-    val descripcion4: String = "b"
-    val descripcion5: String = "Distancia"
+    val description1: String = "SiegeCombat"
+    val description2: String = "a"
+    val description3: String = "CloseCombat"
+    val description4: String = "b"
+    val description5: String = "RangedCombat"
 
-    val fuerza1: Int = 50
-    val fuerza2: Int = 90
-    val fuerza3: Int = 35
-    val fuerza4: Int = 70
-    val fuerza5: Int = 45
-    val fuerza6: Int = 25
+    val power1: Int = 50
+    val power2: Int = 90
+    val power3: Int = 35
+    val power4: Int = 70
+    val power5: Int = 45
+    val power6: Int = 25
 
-    var carta1: SiegeCombatCard = _  // null
-    var carta2: WeatherCard = _
-    var carta3: CloseCombatCard = _
-    var carta4: RangedCombatCard = _
-    var carta5: WeatherCard = _
-    var carta6: SiegeCombatCard = _
-    var carta7: CloseCombatCard = _
-    var carta8: RangedCombatCard = _
+    var card1: SiegeCombatCard = _  // null
+    var card2: WeatherCard = _
+    var card3: CloseCombatCard = _
+    var card4: RangedCombatCard = _
+    var card5: WeatherCard = _
+    var card6: SiegeCombatCard = _
+    var card7: CloseCombatCard = _
+    var card8: RangedCombatCard = _
 
 
     override def beforeEach(context: BeforeEach): Unit = {
-        carta1 = new SiegeCombatCard("carta1", "Asedio", 50)
-        carta2 = new WeatherCard("carta2", "a")
-        carta3 = new CloseCombatCard("carta3", "Cuerpo a Cuerpo", 90)
-        carta4 = new RangedCombatCard("carta4", "Distancia", 35)
-        carta5 = new WeatherCard("carta5", "b")
-        carta6 = new SiegeCombatCard("carta6", "Asedio", 70)
-        carta7 = new CloseCombatCard("carta7", "Cuerpo a Cuerpo", 45)
-        carta8 = new RangedCombatCard("carta8", "Distancia", 25)
+        card1 = new SiegeCombatCard("card1", "SiegeCombat", 50)
+        card2 = new WeatherCard("card2", "a")
+        card3 = new CloseCombatCard("card3", "CloseCombat", 90)
+        card4 = new RangedCombatCard("card4", "RangedCombat", 35)
+        card5 = new WeatherCard("card5", "b")
+        card6 = new SiegeCombatCard("card6", "SiegeCombat", 70)
+        card7 = new CloseCombatCard("card7", "CloseCombat", 45)
+        card8 = new RangedCombatCard("card8", "RangedCombat", 25)
     }
 
     test("Equals"){
-        carta1.equals(carta1)
-        carta2.equals(carta2)
-        carta3.equals(carta3)
-        carta4.equals(carta4)
-        carta5.equals(carta5)
-        carta6.equals(carta6)
-        carta7.equals(carta7)
-        carta8.equals(carta8)
+        card1.equals(card1)
+        card2.equals(card2)
+        card3.equals(card3)
+        card4.equals(card4)
+        card5.equals(card5)
+        card6.equals(card6)
+        card7.equals(card7)
+        card8.equals(card8)
 
-        assertEquals(carta1, new SiegeCombatCard("carta1", "Asedio", 50))
-        assertEquals(carta2, new WeatherCard("carta2", "a"))
-        assertEquals(carta3, new CloseCombatCard("carta3", "Cuerpo a Cuerpo", 90))
-        assertEquals(carta4, new RangedCombatCard("carta4", "Distancia", 35))
-        assertEquals(carta5, new WeatherCard("carta5", "b"))
-        assertEquals(carta6, new SiegeCombatCard("carta6", "Asedio", 70))
-        assertEquals(carta7, new CloseCombatCard("carta7", "Cuerpo a Cuerpo", 45))
-        assertEquals(carta8, new RangedCombatCard("carta8", "Distancia", 25))
+        assertEquals(card1, new SiegeCombatCard("card1", "SiegeCombat", 50))
+        assertEquals(card2, new WeatherCard("card2", "a"))
+        assertEquals(card3, new CloseCombatCard("card3", "CloseCombat", 90))
+        assertEquals(card4, new RangedCombatCard("card4", "RangedCombat", 35))
+        assertEquals(card5, new WeatherCard("card5", "b"))
+        assertEquals(card6, new SiegeCombatCard("card6", "SiegeCombat", 70))
+        assertEquals(card7, new CloseCombatCard("card7", "CloseCombat", 45))
+        assertEquals(card8, new RangedCombatCard("card8", "RangedCombat", 25))
 
-        assert(!carta1.equals(new WeatherCard("carta2", "a")))
-        assert(!carta2.equals(new CloseCombatCard("carta3", "Cuerpo a Cuerpo", 90)))
-        assert(!carta3.equals(new RangedCombatCard("carta4", "Distancia", 35)))
-        assert(!carta4.equals(new WeatherCard("carta5", "b")))
-        assert(!carta5.equals(new SiegeCombatCard("carta6", "Asedio", 70)))
-        assert(!carta6.equals(new CloseCombatCard("carta7", "Cuerpo a Cuerpo", 45)))
-        assert(!carta7.equals(new RangedCombatCard("carta8", "Distancia", 25)))
-        assert(!carta8.equals(new SiegeCombatCard("carta1", "Asedio", 50)))
+        assert(!card1.equals(new WeatherCard("card2", "a")))
+        assert(!card2.equals(new CloseCombatCard("card3", "CloseCombat", 90)))
+        assert(!card3.equals(new RangedCombatCard("card4", "RangedCombat", 35)))
+        assert(!card4.equals(new WeatherCard("card5", "b")))
+        assert(!card5.equals(new SiegeCombatCard("card6", "SiegeCombat", 70)))
+        assert(!card6.equals(new CloseCombatCard("card7", "CloseCombat", 45)))
+        assert(!card7.equals(new RangedCombatCard("card8", "RangedCombat", 25)))
+        assert(!card8.equals(new SiegeCombatCard("card1", "SiegeCombat", 50)))
 
-        assertEquals(carta1, carta1)
-        assertEquals(carta2, carta2)
-        assertEquals(carta3, carta3)
-        assertEquals(carta4, carta4)
-        assertEquals(carta5, carta5)
-        assertEquals(carta6, carta6)
-        assertEquals(carta7, carta7)
-        assertEquals(carta8, carta8)
-        assert(!carta1.equals(carta2))
-        assert(!carta1.equals(carta3))
-        assert(!carta1.equals(carta4))
-        assert(!carta1.equals(carta5))
-        assert(!carta1.equals(carta6))
-        assert(!carta1.equals(carta7))
-        assert(!carta1.equals(carta8))
-        assert(!carta2.equals(carta3))
-        assert(!carta2.equals(carta4))
-        assert(!carta2.equals(carta5))
-        assert(!carta2.equals(carta6))
-        assert(!carta2.equals(carta7))
-        assert(!carta2.equals(carta8))
-        assert(!carta3.equals(carta4))
-        assert(!carta3.equals(carta5))
-        assert(!carta3.equals(carta6))
-        assert(!carta3.equals(carta7))
-        assert(!carta3.equals(carta8))
-        assert(!carta4.equals(carta5))
-        assert(!carta4.equals(carta6))
-        assert(!carta4.equals(carta7))
-        assert(!carta4.equals(carta8))
-        assert(!carta5.equals(carta6))
-        assert(!carta5.equals(carta7))
-        assert(!carta5.equals(carta8))
-        assert(!carta6.equals(carta7))
-        assert(!carta6.equals(carta8))
-        assert(!carta7.equals(carta8))
+        assertEquals(card1, card1)
+        assertEquals(card2, card2)
+        assertEquals(card3, card3)
+        assertEquals(card4, card4)
+        assertEquals(card5, card5)
+        assertEquals(card6, card6)
+        assertEquals(card7, card7)
+        assertEquals(card8, card8)
+        assert(!card1.equals(card2))
+        assert(!card1.equals(card3))
+        assert(!card1.equals(card4))
+        assert(!card1.equals(card5))
+        assert(!card1.equals(card6))
+        assert(!card1.equals(card7))
+        assert(!card1.equals(card8))
+        assert(!card2.equals(card3))
+        assert(!card2.equals(card4))
+        assert(!card2.equals(card5))
+        assert(!card2.equals(card6))
+        assert(!card2.equals(card7))
+        assert(!card2.equals(card8))
+        assert(!card3.equals(card4))
+        assert(!card3.equals(card5))
+        assert(!card3.equals(card6))
+        assert(!card3.equals(card7))
+        assert(!card3.equals(card8))
+        assert(!card4.equals(card5))
+        assert(!card4.equals(card6))
+        assert(!card4.equals(card7))
+        assert(!card4.equals(card8))
+        assert(!card5.equals(card6))
+        assert(!card5.equals(card7))
+        assert(!card5.equals(card8))
+        assert(!card6.equals(card7))
+        assert(!card6.equals(card8))
+        assert(!card7.equals(card8))
     }
 
     test("hashCode"){
-        assert(carta1.hashCode()!=carta2.hashCode())
-        assert(carta2.hashCode()!=carta3.hashCode())
-        assert(carta3.hashCode()!=carta4.hashCode())
-        assert(carta4.hashCode()!=carta5.hashCode())
-        assert(carta5.hashCode()!=carta6.hashCode())
-        assert(carta6.hashCode()!=carta7.hashCode())
-        assert(carta7.hashCode()!=carta8.hashCode())
-        assert(carta8.hashCode()!=carta1.hashCode())
+        assert(card1.hashCode()!=card2.hashCode())
+        assert(card2.hashCode()!=card3.hashCode())
+        assert(card3.hashCode()!=card4.hashCode())
+        assert(card4.hashCode()!=card5.hashCode())
+        assert(card5.hashCode()!=card6.hashCode())
+        assert(card6.hashCode()!=card7.hashCode())
+        assert(card7.hashCode()!=card8.hashCode())
+        assert(card8.hashCode()!=card1.hashCode())
         
-        assert(carta1.hashCode() == (new SiegeCombatCard("carta1", "Asedio", 50)).hashCode())
-        assert(carta2.hashCode() == (new WeatherCard("carta2", "a")).hashCode())
-        assert(carta3.hashCode() == (new CloseCombatCard("carta3", "Cuerpo a Cuerpo", 90)).hashCode())
-        assert(carta4.hashCode() == (new RangedCombatCard("carta4", "Distancia", 35)).hashCode())
-        assert(carta5.hashCode() == (new WeatherCard("carta5", "b")).hashCode())
-        assert(carta6.hashCode() == (new SiegeCombatCard("carta6", "Asedio", 70)).hashCode())
-        assert(carta7.hashCode() == (new CloseCombatCard("carta7", "Cuerpo a Cuerpo", 45)).hashCode())
-        assert(carta8.hashCode() == (new RangedCombatCard("carta8", "Distancia", 25)).hashCode())
+        assert(card1.hashCode() == (new SiegeCombatCard("card1", "SiegeCombat", 50)).hashCode())
+        assert(card2.hashCode() == (new WeatherCard("card2", "a")).hashCode())
+        assert(card3.hashCode() == (new CloseCombatCard("card3", "CloseCombat", 90)).hashCode())
+        assert(card4.hashCode() == (new RangedCombatCard("card4", "RangedCombat", 35)).hashCode())
+        assert(card5.hashCode() == (new WeatherCard("card5", "b")).hashCode())
+        assert(card6.hashCode() == (new SiegeCombatCard("card6", "SiegeCombat", 70)).hashCode())
+        assert(card7.hashCode() == (new CloseCombatCard("card7", "CloseCombat", 45)).hashCode())
+        assert(card8.hashCode() == (new RangedCombatCard("card8", "RangedCombat", 25)).hashCode())
     }
 
-    test("Nombre") {
-        assertEquals(carta1.nombre, nombre1)
-        assertEquals(carta2.nombre, nombre2)
-        assertEquals(carta3.nombre, nombre3)
-        assertEquals(carta4.nombre, nombre4)
-        assertEquals(carta5.nombre, nombre5)
-        assertEquals(carta6.nombre, nombre6)
-        assertEquals(carta7.nombre, nombre7)
-        assertEquals(carta8.nombre, nombre8)
+    test("Name") {
+        assertEquals(card1.name, name1)
+        assertEquals(card2.name, name2)
+        assertEquals(card3.name, name3)
+        assertEquals(card4.name, name4)
+        assertEquals(card5.name, name5)
+        assertEquals(card6.name, name6)
+        assertEquals(card7.name, name7)
+        assertEquals(card8.name, name8)
 
-        assert(!carta1.nombre.equals(nombre2))
-        assert(!carta2.nombre.equals(nombre3))
-        assert(!carta3.nombre.equals(nombre4))
-        assert(!carta4.nombre.equals(nombre5))
-        assert(!carta5.nombre.equals(nombre6))
-        assert(!carta6.nombre.equals(nombre7))
-        assert(!carta7.nombre.equals(nombre8))
-        assert(!carta8.nombre.equals(nombre1))
+        assert(!card1.name.equals(name2))
+        assert(!card2.name.equals(name3))
+        assert(!card3.name.equals(name4))
+        assert(!card4.name.equals(name5))
+        assert(!card5.name.equals(name6))
+        assert(!card6.name.equals(name7))
+        assert(!card7.name.equals(name8))
+        assert(!card8.name.equals(name1))
     }
 
-    test("Descripcion") {
-        assertEquals(carta1.descripcion, descripcion1)
-        assertEquals(carta2.descripcion, descripcion2)
-        assertEquals(carta3.descripcion, descripcion3)
-        assertEquals(carta4.descripcion, descripcion5)
-        assertEquals(carta5.descripcion, descripcion4)
-        assertEquals(carta6.descripcion, descripcion1)
-        assertEquals(carta7.descripcion, descripcion3)
-        assertEquals(carta8.descripcion, descripcion5)
+    test("Description") {
+        assertEquals(card1.description, description1)
+        assertEquals(card2.description, description2)
+        assertEquals(card3.description, description3)
+        assertEquals(card4.description, description5)
+        assertEquals(card5.description, description4)
+        assertEquals(card6.description, description1)
+        assertEquals(card7.description, description3)
+        assertEquals(card8.description, description5)
 
-        assert(!carta1.nombre.equals(descripcion2))
-        assert(!carta2.nombre.equals(descripcion3))
-        assert(!carta3.nombre.equals(descripcion4))
-        assert(!carta4.nombre.equals(descripcion2))
-        assert(!carta5.nombre.equals(descripcion3))
-        assert(!carta6.nombre.equals(descripcion4))
-        assert(!carta7.nombre.equals(descripcion5))
-        assert(!carta8.nombre.equals(descripcion1))
+        assert(!card1.name.equals(description2))
+        assert(!card2.name.equals(description3))
+        assert(!card3.name.equals(description4))
+        assert(!card4.name.equals(description2))
+        assert(!card5.name.equals(description3))
+        assert(!card6.name.equals(description4))
+        assert(!card7.name.equals(description5))
+        assert(!card8.name.equals(description1))
     }
 
-    test("Fuerza") {
-        assertEquals(carta1.fuerza, fuerza1)
-        assertEquals(carta3.fuerza, fuerza2)
-        assertEquals(carta4.fuerza, fuerza3)
-        assertEquals(carta6.fuerza, fuerza4)
-        assertEquals(carta7.fuerza, fuerza5)
-        assertEquals(carta8.fuerza, fuerza6)
+    test("Power") {
+        assertEquals(card1.power, power1)
+        assertEquals(card3.power, power2)
+        assertEquals(card4.power, power3)
+        assertEquals(card6.power, power4)
+        assertEquals(card7.power, power5)
+        assertEquals(card8.power, power6)
 
-        assert(!carta1.fuerza.equals(fuerza2))
-        assert(!carta3.fuerza.equals(fuerza3))
-        assert(!carta4.fuerza.equals(fuerza4))
-        assert(!carta6.fuerza.equals(fuerza5))
-        assert(!carta7.fuerza.equals(fuerza6))
-        assert(!carta8.fuerza.equals(fuerza1))
+        assert(!card1.power.equals(power2))
+        assert(!card3.power.equals(power3))
+        assert(!card4.power.equals(power4))
+        assert(!card6.power.equals(power5))
+        assert(!card7.power.equals(power6))
+        assert(!card8.power.equals(power1))
     }
 
-    test("Definiciones") {
-        assert(!carta1.fuerza.equals(descripcion1))
-        assert(!carta2.nombre.equals(fuerza6))
-        assert(!carta3.fuerza.equals(nombre3))
-        assert(!carta4.fuerza.equals(descripcion2))
-        assert(!carta5.descripcion.equals(descripcion1))
-        assert(!carta6.nombre.equals(fuerza4))
-        assert(!carta7.fuerza.equals(descripcion3))
-        assert(!carta8.descripcion.equals(fuerza2))
+    test("Definitions") {
+        assert(!card1.power.equals(name1))
+        assert(!card2.name.equals(power6))
+        assert(!card3.power.equals(name3))
+        assert(!card4.power.equals(description2))
+        assert(!card5.description.equals(description1))
+        assert(!card6.name.equals(power4))
+        assert(!card7.power.equals(description3))
+        assert(!card8.description.equals(power2))
     }
 
-    test("Jogador") {
-        assert(!carta1.equals(new Player("Simba", new boardSection(), 90, List[Card](), List[Card]())))
-        assert(!carta2.equals(new Player("Kitty", new boardSection(), 100, List[Card](), List[Card]())))
+    test("Player") {
+        assert(!card1.equals(new Player("Simba", new boardSection(), 2, List[Card](), List[Card]())))
+        assert(!card2.equals(new Player("Kitty", new boardSection(), 2, List[Card](), List[Card]())))
     }
 
 }
