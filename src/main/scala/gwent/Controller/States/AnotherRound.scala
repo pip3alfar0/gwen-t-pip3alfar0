757e130a2b78819b5ec3cb3e9b5168304e9a233f
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-13 18:20:30 -0400

    FEAT: changed the language from spanish to english

--- src/main/scala/gwent/Controller/States/AnotherRound.scala
+++ src/main/scala/gwent/Controller/States/AnotherRound.scala
@@ -4,14 +4,14 @@ package gwent.Controller.States
 import gwent.Controller.GameController
 
 
-class OtroRound(context: GameController) extends GameState(context) {
+class AnotherRound(context: GameController) extends GameState(context) {
 
-  override def toJugadorJuega(): Unit = {
-    context.estado = new JugadorJuega(context)
+  override def toPlayerTurn(): Unit = {
+    context.state = new PlayerTurn(context)
   }
 
-  override def toComputadorJuega(): Unit = {
-    context.estado = new ComputadorJuega(context)
+  override def toComputerTurn(): Unit = {
+    context.state = new ComputerTurn(context)
   }
   
 }
