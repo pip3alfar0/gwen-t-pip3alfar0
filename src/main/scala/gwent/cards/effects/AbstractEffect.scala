306b3dd7438ad4ceeca4d84a203867d63e0e98cb
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 19:56:04 -0400

    FEAT: created the new abstract class AbstractEffect

--- src/main/scala/gwent/cards/effects/AbstractEffect.scala
+++ src/main/scala/gwent/cards/effects/AbstractEffect.scala
@@ -9,16 +9,26 @@ import gwent.cards.unit.UnitCard
 /** It's an abstract class that represents
  * 
  * */
-abstract class AbstractWeatherEffect extends Effect {
-
+abstract class AbstractEffect extends Effect {
+  
+  /** Set the current power to 1 of some list of unit cards */
+  def cPower(t1: List[UnitCard], t2: List[UnitCard]): Unit = {
+    for (c <- t1) {
+      c.currentPower = 1
+    }
+    for (c <- t2) {
+      c.currentPower = 1
+    }
+  }
+  
   /** Method apply for the weather effects */
   def apply(self: Card, target1: BoardSection, target2: BoardSection): Unit = {
-    // do nothing
+    // do nothing for now
   }
 
   /** Method apply for the unit effects */
   def apply(self: Card, target: List[UnitCard]): Unit = {
-    // do nothing
+    // do nothing for now
   }
   
 }
