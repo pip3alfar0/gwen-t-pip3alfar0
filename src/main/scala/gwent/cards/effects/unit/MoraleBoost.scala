f3e4c849000f2b953c708762ff7195b7b24e5c05
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 19:58:43 -0400

    FEAT: created the new case class MoraleBoost as a Unit card effect with the method apply

--- src/main/scala/gwent/cards/effects/unit/MoraleBoost.scala
+++ src/main/scala/gwent/cards/effects/unit/MoraleBoost.scala
@@ -8,7 +8,7 @@ import gwent.cards.effects.AbstractEffect
 /** It's a class that represents the unit card effect called Morale Booster
  *
  * */
-case class MoraleBooster() extends AbstractEffect {
+case class MoraleBoost() extends AbstractEffect {
 
   /** The power of all the cards on the board's section where the card was played increment 1 */
   override def apply(self: Card, target: List[UnitCard]): Unit = {
