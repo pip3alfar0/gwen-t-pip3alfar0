1b59c7bf57b04eeb79e570c00028f4e00550b3ed
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 19:37:58 -0400

    FEAT: changed method doAction for the class PlayerTurn

--- src/main/scala/gwent/controller/states/PlayerTurn.scala
+++ src/main/scala/gwent/controller/states/PlayerTurn.scala
@@ -1,23 +1,31 @@
 package cl.uchile.dcc
-package gwent.Controller.States
+package gwent.controller.states
 
-import gwent.Controller.GameController
+import gwent.controller.GameController
 
 
 class PlayerTurn(context: GameController) extends GameState(context) {
 
+  /** The player decided to pass */
   override def toOtherTurn(): Unit = {
     context.state = new OtherTurn(context)
   }
 
+  /** The player played a card, so it's the computer's turn */
   override def toComputerTurn(): Unit = {
     context.state = new ComputerTurn(context)
   }
 
-  // tiene que jugar una carta, o pasar
+  /** The player decides if he want to play a card or pass. If he choose to play, he decides which card it's going to be played */
   override def doAction(): Unit = {
-    context.PlayPlayer()
+    println("Do you want to play or pass?")
+    val decision: String = scala.io.StdIn.readLine()
+    if (decision == "Play") {
+      context.PlayPlayer()
+    } else {
+      println("You passed. It's the opponent's turn")
+      context.passPlayer()
+    }
   }
 
 }
-
