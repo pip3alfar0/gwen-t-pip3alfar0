2408c4f076bf3dfc98bf08eae2b76350f5308464
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 19:57:36 -0400

    FEAT: created the new class NullEffect with the null methods apply for the weather and unit effects

--- src/main/scala/gwent/cards/effects/NullEffect.scala
+++ src/main/scala/gwent/cards/effects/NullEffect.scala
@@ -1,5 +1,21 @@
-package cl.uchile.dcc.gwent.cards.effects
+package cl.uchile.dcc
+package gwent.cards.effects
 
-class NullEffect {
+import gwent.cards.unit.UnitCard
+import gwent.cards.Card
+import gwent.board.BoardSection
+
+
+case class NullEffect() extends Effect {
+  
+  /** Method apply for the weather effects */
+  def apply(self: Card, target1: BoardSection, target2: BoardSection): Unit = {
+    // do nothing
+  }
+
+  /** Method apply for the unit effects */
+  def apply(self: Card, target: List[UnitCard]): Unit = {
+    // do nothing
+  }
 
 }
