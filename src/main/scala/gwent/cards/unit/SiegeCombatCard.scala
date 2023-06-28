e9c232820b64a23af357e2558dbc5ad10a8cffe6
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 19:48:40 -0400

    FEAT: added the val effect on the class SiegeCombatCard

--- src/main/scala/gwent/cards/unit/SiegeCombatCard.scala
+++ src/main/scala/gwent/cards/unit/SiegeCombatCard.scala
@@ -1,10 +1,11 @@
 package cl.uchile.dcc
-package gwent.Cards.Unit
+package gwent.cards.unit
 
-import gwent.Cards.Unit.AbstractUnitCard
+import gwent.cards.unit.AbstractUnitCard
+import gwent.player.Player
+import gwent.board.Board
 
-import gwent.Player.Player
-import gwent.Board.Board
+import cl.uchile.dcc.gwent.cards.effects.Effect
 
 /** Class representing a siege combat unit card in the game.
  *
@@ -21,7 +22,8 @@ import gwent.Board.Board
  * @author Felipe Alfaro
  */
 
-class SiegeCombatCard(name: String, description: String, power: Int) extends AbstractUnitCard(name, description, power) {
+class SiegeCombatCard(name: String, description: String, 
+                      power: Int, effect: Effect) extends AbstractUnitCard(name, description, power, effect) {
 
   /** Play a siege combat card and added to the siege combat card zone.
    *
