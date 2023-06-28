adc86bcb66aa6022803ae1c2f1fc69b84c573fa5
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 19:59:36 -0400

    FEAT: created the new case class BitingFrost as a Weather card effect with the method apply

--- src/main/scala/gwent/cards/effects/weather/BitingFrost.scala
+++ src/main/scala/gwent/cards/effects/weather/BitingFrost.scala
@@ -1,5 +1,20 @@
-package cl.uchile.dcc.gwent.Cards.Effects.Weather
+package cl.uchile.dcc
+package gwent.cards.effects.weather
 
-class BitingFrost {
+import gwent.cards.Card
+import gwent.board.BoardSection
+import gwent.cards.effects.AbstractEffect
+
+/** It's a class that represents the weather effect called Torrential Rain
+ *
+ * */
+case class BitingFrost() extends AbstractEffect {
+
+  /** The power of all the close combat cards on the board is 1 */
+  override def apply(self: Card, target1: BoardSection, target2: BoardSection): Unit = {
+    val t1 = target1.close
+    val t2 = target2.close
+    cPower(t1, t2)
+  }
 
 }
