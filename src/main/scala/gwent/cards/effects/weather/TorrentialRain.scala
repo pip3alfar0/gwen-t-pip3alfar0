e5b1742a6c7830f668c3c552d68dbaecfa4feb12
pip3alfar0 <alfaro.feli@gmail.com>
2023-06-28 20:00:36 -0400

    FEAT: created the new case class TorrentialRain as a Weather card effect with the method apply

--- src/main/scala/gwent/cards/effects/weather/TorrentialRain.scala
+++ src/main/scala/gwent/cards/effects/weather/TorrentialRain.scala
@@ -1,5 +1,20 @@
-package cl.uchile.dcc.gwent.Cards.Effects.Weather
+package cl.uchile.dcc
+package gwent.cards.effects.weather
 
-class TorrentialRain {
+import gwent.cards.Card
+import gwent.board.BoardSection
+import gwent.cards.effects.AbstractEffect
+
+/** It's a class that represents the weather effect called Torrential Rain
+* 
+* */
+case class TorrentialRain() extends AbstractEffect {
+
+  /** The power of all the siege cards on the board is 1 */
+  override def apply(self: Card, target1: BoardSection, target2: BoardSection): Unit = {
+    val t1 = target1.siege
+    val t2 = target2.siege
+    cPower(t1, t2)
+  }
 
 }
