baf8a8ff596b77b1f2dcc1a5494a0cadcc920705
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-10 18:01:10 -0400

    FEAT: deleted the method doAction for the class EndGame

--- src/main/scala/gwent/controller/states/EndGame.scala
+++ src/main/scala/gwent/controller/states/EndGame.scala
@@ -5,7 +5,12 @@ import gwent.controller.GameController
 
 
 class EndGame(context: GameController) extends GameState(context) {
-  
+
   // jugador que le queden gemas gana, si ninguno tiene es empate
+  /*
+  override def doAction(): Unit = {
+    context.finalGemCount()
+  }
+  */
   
 }
