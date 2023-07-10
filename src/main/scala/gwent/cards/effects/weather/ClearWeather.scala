54b00a9c7958dafcae3200b9d5a126fcd1f9c9d8
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-10 17:47:56 -0400

    FEAT: changed the method apply for the case class ClearWeather, now applies on the weatherPower

--- src/main/scala/gwent/cards/effects/weather/ClearWeather.scala
+++ src/main/scala/gwent/cards/effects/weather/ClearWeather.scala
@@ -6,17 +6,16 @@ import gwent.board.BoardSection
 import gwent.cards.effects.AbstractEffect
 
 /** It's a class that represents the weather effect called Torrential Rain
- *
  * */
 case class ClearWeather() extends AbstractEffect {
 
   /** The effect of all the weather cards are null */
   override def apply(self: Card, target1: BoardSection, target2: BoardSection): Unit = {
     for (c <- target1.siege) {
-      c.currentPower = c.power
+      c.weatherPower = c.currentPower
     }
     for (c <- target2.siege) {
-      c.currentPower = c.power
+      c.weatherPower = c.currentPower
     }
   }
 
