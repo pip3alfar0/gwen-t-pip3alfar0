99a0edd1bb66d3c144512e229b0283d7c30245a5
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-17 17:26:51 -0400

    FEAT: changed the old method apply for the case class ClearWeather

--- src/main/scala/gwent/cards/effects/weather/ClearWeather.scala
+++ src/main/scala/gwent/cards/effects/weather/ClearWeather.scala
@@ -11,12 +11,18 @@ case class ClearWeather() extends AbstractEffect {
 
   /** The effect of all the weather cards are null */
   override def apply(self: Card, target1: BoardSection, target2: BoardSection): Unit = {
-    for (c <- target1.siege) {
-      c.weatherPower = c.currentPower
-    }
-    for (c <- target2.siege) {
-      c.weatherPower = c.currentPower
-    }
+    val t1s = target1.siege
+    val t2s = target2.siege
+    val t1r = target1.ranged
+    val t2r = target2.ranged
+    val t1c = target1.close
+    val t2c = target2.close
+    cClear(t1s)
+    cClear(t2s)
+    cClear(t1r)
+    cClear(t2r)
+    cClear(t1c)
+    cClear(t2c)
   }
 
 }
