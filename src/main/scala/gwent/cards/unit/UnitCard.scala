8a08a19ea800a8e1d2c6b52d6c04815d914b3169
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-17 17:21:36 -0400

    FEAT: added the var previousPower, weatherEffect and unitEffect for the trait UnitCard

--- src/main/scala/gwent/cards/unit/UnitCard.scala
+++ src/main/scala/gwent/cards/unit/UnitCard.scala
@@ -31,11 +31,23 @@ trait UnitCard extends Card {
    */
   var currentPower: Int = power
 
-  /** The weather power of the card is the changes of the card's power when a weather card is played
-   * on the game.
+  /** The previous power of the card is the previous current power before suffered changes.
    *
-   * Initially set to the base [[currentPower]] of the card.
+   * Initially set as 0.
    */
-  var weatherPower: Int = currentPower
+  var previousPower: Int = 0
+
+
+  /** The weather effect it's the way to know if the card suffered changes from weather cards.
+   *
+   * Initially set as 0 and changes to 1 if some weather effect applies on the card.
+   */
+  var weatherEffect: Int = 0
+
+  /** The unit effect is the way to know how many changes suffered the card from unit card effect.
+   *
+   * Initially set as 0.
+   */
+  var unitEffect: Int = 0
 
 }
