1307df79b88f36437f4c6b02a8b949d1d78be3ea
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-19 21:48:56 -0400

    DOC: added some documentation on the class EndGame

--- src/main/scala/gwent/controller/states/EndGame.scala
+++ src/main/scala/gwent/controller/states/EndGame.scala
@@ -3,15 +3,16 @@ package gwent.controller.states
 
 import gwent.controller.GameController
 
-
+/** Class representing the end of the game.
+ *
+ * A `EndGame` is a type of [[GameState]].
+ * This state represents the moment when at least one of the player have zero gems, the player with at least one gem won.
+ * If non on the players have gems, it's a tie.
+ *
+ * @param context The context of the game.
+ * @author Felipe Alfaro
+ */
 class EndGame(context: GameController) extends GameState(context) {
-
-  // jugador que le queden gemas gana, si ninguno tiene es empate
-  /*
-  override def doAction(): Unit = {
-    context.finalGemCount()
-  }
-  */
 
   override def isInEndGame: Boolean = {
     true
