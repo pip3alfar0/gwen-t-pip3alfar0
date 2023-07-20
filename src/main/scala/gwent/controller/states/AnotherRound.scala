e21738567655e5f06d1ed05e092d1b2438eacb70
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-19 21:49:42 -0400

    DOC: added some documentation on the class AnotherRound

--- src/main/scala/gwent/controller/states/AnotherRound.scala
+++ src/main/scala/gwent/controller/states/AnotherRound.scala
@@ -3,13 +3,22 @@ package gwent.controller.states
 
 import gwent.controller.GameController
 
-
+/** Class representing the start of another round.
+ *
+ * A `AnotherRound` is a type of [[GameState]].
+ * This state represents when the players still have at least one gem, so they are going to play another round.
+ *
+ * @param context The context of the game.
+ * @author Felipe Alfaro
+ */
 class AnotherRound(context: GameController) extends GameState(context) {
 
+  /** If the computer was the initial player, the player starts */
   override def toPlayerTurn(): Unit = {
     context.state = new PlayerTurn(context)
   }
 
+  /** If the player was the initial player, the computer starts */
   override def toComputerTurn(): Unit = {
     context.state = new ComputerTurn(context)
   }
