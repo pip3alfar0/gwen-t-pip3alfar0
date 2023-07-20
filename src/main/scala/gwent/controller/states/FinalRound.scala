f96a2629e91612f9781f1f1a3ba41e4c92e37818
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-19 21:51:57 -0400

    DOC: added some documentation on the class FinalRound

--- src/main/scala/gwent/controller/states/FinalRound.scala
+++ src/main/scala/gwent/controller/states/FinalRound.scala
@@ -3,12 +3,22 @@ package gwent.controller.states
 
 import gwent.controller.GameController
 
+/** Class representing the end of the round.
+ *
+ * A `FinalState` is a type of [[GameState]].
+ * This state represents the end of a round, in this state is where it's determined if another round is played or the game is over.
+ *
+ * @param context The context of the game.
+ * @author Felipe Alfaro
+ */
 class FinalRound(context: GameController) extends GameState(context) {
 
+  /** One of the players don't have any gems */
   override def toEndGame(): Unit = {
     context.state = new EndGame(context)
   }
 
+  /** The players still have at least one gem */
   override def toAnotherRound(): Unit = {
     context.state = new AnotherRound(context)
   }
