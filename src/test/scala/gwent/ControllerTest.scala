c4e380807ccba2f72540e5c6732bb7610a38efe5
pip3alfar0 <alfaro.feli@gmail.com>
2023-07-19 23:33:47 -0400

    TEST: changed some old tests

--- src/test/scala/gwent/ControllerTest.scala
+++ src/test/scala/gwent/ControllerTest.scala
@@ -12,12 +12,17 @@ import gwent.player.Player
 import gwent.board.*
 import gwent.controller.GameController
 
+import cl.uchile.dcc.gwent.exception.{InvalidMethodException, InvalidTransitionException}
+import org.junit.Assert
+
 
 class ControllerTest extends munit.FunSuite {
   var controller: GameController = _
+  var controller2: GameController = _
 
   override def beforeEach(context: BeforeEach): Unit = {
     controller = new GameController
+    controller2 = new GameController
   }
 
   test("Starts") {
@@ -36,6 +41,7 @@ class ControllerTest extends munit.FunSuite {
     controller.Starts("Simba")
     if (controller.state.isInComputerTurn) {
       controller.state.toPlayerTurn()
+      controller.initialPlayer_=(controller.player.get)
       controller.currentPlayer_=(controller.player.get)
     }
     controller.PlayPlayer(controller.player.get.handCards(1))
@@ -50,6 +56,7 @@ class ControllerTest extends munit.FunSuite {
     controller.Starts("Kitty")
     if (controller.state.isInComputerTurn) {
       controller.state.toPlayerTurn()
+      controller.initialPlayer_=(controller.player.get)
       controller.currentPlayer_=(controller.player.get)
     }
     controller.PassPlayer()
@@ -61,6 +68,7 @@ class ControllerTest extends munit.FunSuite {
     controller.Starts("Miguelito")
     if (controller.state.isInPlayerTurn) {
       controller.state.toComputerTurn()
+      controller.initialPlayer_=(controller.computer.get)
       controller.currentPlayer_=(controller.computer.get)
     }
     controller.PassComputer()
@@ -92,6 +100,7 @@ class ControllerTest extends munit.FunSuite {
     controller.Starts("Miguelito")
     if (controller.state.isInComputerTurn) {
       controller.state.toPlayerTurn()
+      controller.initialPlayer_=(controller.player.get)
       controller.currentPlayer_=(controller.player.get)
     }
     controller.PlayPlayer(controller.player.get.handCards(1))
@@ -110,10 +119,11 @@ class ControllerTest extends munit.FunSuite {
     assertEquals(controller.computer.get.handCards.length, 6)
   }
 
-  test("Winner") {
+  test("Winner 1") {
     controller.Starts("Miguelito")
     if (controller.state.isInComputerTurn) {
       controller.state.toPlayerTurn()
+      controller.initialPlayer_=(controller.player.get)
       controller.currentPlayer_=(controller.player.get)
     }
     controller.PlayPlayer(controller.player.get.handCards(1))
@@ -135,6 +145,63 @@ class ControllerTest extends munit.FunSuite {
     controller.PlayComputer()
     controller.PassComputer()
     controller.gems()
+  }
+
+  test("Winner 2") {
+    controller2.Starts("Rene Bridge")
+    if (controller2.state.isInPlayerTurn) {
+      controller2.state.toComputerTurn()
+      controller2.initialPlayer_=(controller2.computer.get)
+      controller2.currentPlayer_=(controller2.computer.get)
+    }
+    controller2.PlayComputer()
+    controller2.PlayPlayer(controller2.player.get.handCards(1))
+    controller2.PlayComputer()
+    controller2.PlayPlayer(controller2.player.get.handCards(1))
+    controller2.PlayComputer()
+    controller2.PlayPlayer(controller2.player.get.handCards(1))
+    controller2.PassComputer()
+    controller2.PlayPlayer(controller2.player.get.handCards(1))
+    controller2.PlayPlayer(controller2.player.get.handCards(1))
+    controller2.PassPlayer()
+    controller2.gems()
+    controller2.AnotherRound()
+    controller2.PlayPlayer(controller2.player.get.handCards(1))
+    controller2.PassComputer()
+    controller2.PassPlayer()
+    controller2.gems()
+  }
+
+  test("Exceptions") {
+    controller2.Starts("Rene Bridge")
+    if (controller2.state.isInComputerTurn) {
+      controller2.state.toPlayerTurn()
+      controller2.initialPlayer_=(controller2.player.get)
+      controller2.currentPlayer_=(controller2.player.get)
+    }
+    val e1 = Assert.assertThrows(classOf[InvalidMethodException], () => controller2.PlayComputer())
+    assertEquals(e1.getMessage, "It's the player's turn, not yours")
+
+
+    controller.Starts("Kike Morande")
+    if (controller.state.isInPlayerTurn) {
+      controller.state.toComputerTurn()
+      controller.initialPlayer_=(controller.computer.get)
+      controller.currentPlayer_=(controller.computer.get)
+    }
+    val e2 = Assert.assertThrows(classOf[InvalidMethodException], () => controller.PlayPlayer(controller.player.get.handCards(1)))
+    assertEquals(e2.getMessage, "It's the computer's turn, not yours")
+  }
+  
+  test("Transition error") {
+    controller.Starts("Kike Morande")
+    if (controller.state.isInPlayerTurn) {
+      controller.state.toComputerTurn()
+      controller.initialPlayer_=(controller.computer.get)
+      controller.currentPlayer_=(controller.computer.get)
+    }
+    val e1 = Assert.assertThrows(classOf[InvalidMethodException], () => controller.PlayComputer())
+    assertEquals(e1.getMessage, "Cannot transition from  to ")
   }
 
 }
